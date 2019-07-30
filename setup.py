c564e72c985522f0a5d691b69c0107cc6e4f3649
Daniel Ecer <de-code@users.noreply.github.com>
2019-07-30 20:50:27 +0100

    added setup.py for dataflow

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+
+from setuptools import (
+    find_packages,
+    setup
+)
+
+with open(os.path.join('requirements.txt'), 'r') as f:
+    REQUIRED_PACKAGES = f.readlines()
+
+with open(os.path.join('requirements.links.txt'), 'r') as f:
+    LINKS_PACKAGES = f.readlines()
+
+packages = find_packages()
+
+setup(
+    name='sciencebeam_trainer_grobid_tools',
+    version='0.0.1',
+    install_requires=REQUIRED_PACKAGES + LINKS_PACKAGES,
+    packages=packages,
+    include_package_data=True,
+    description='ScienceBeam Trainer - Tools for GROBID training',
+)
