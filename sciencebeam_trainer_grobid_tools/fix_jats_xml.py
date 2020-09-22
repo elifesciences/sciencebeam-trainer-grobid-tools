c35cc230f60427b2bdd23f78d2be2f34e8013814
Daniel Ecer <de-code@users.noreply.github.com>
2020-09-22 14:15:30 +0100

    implemented cli

--- sciencebeam_trainer_grobid_tools/fix_jats_xml.py
+++ sciencebeam_trainer_grobid_tools/fix_jats_xml.py
@@ -1,12 +1,22 @@
+import argparse
 import logging
 import re
+from pathlib import Path
 
 from lxml import etree
+
+from sciencebeam_trainer_grobid_tools.utils.xml import parse_xml
+
+from sciencebeam_trainer_grobid_tools.auto_annotate_utils import (
+    add_debug_argument,
+    process_debug_argument
+)
 
 
 LOGGER = logging.getLogger(__name__)
 
 
+REF_XPATH = './/back/ref-list/ref'
 DOI_XPATH = './/pub-id[@pub-id-type="doi"]'
 
 DOI_PATTERN = r'\b(10.\d{4,}/[^\[]+)'
@@ -38,3 +48,57 @@ def fix_doi(reference_element: etree.Element) -> etree.Element:
         add_text_to_previous(doi_element, doi_text[:m.start(1)])
         add_text_to_tail_prefix(doi_element, doi_text[m.start(1) + len(matching_doi):])
     return reference_element
+
+
+def fix_jats_xml_node(root: etree.Element):
+    for ref in root.xpath(REF_XPATH):
+        fix_doi(ref)
+    return root
+
+
+def fix_jats_xml_file(input_file: str, output_file: str):
+    LOGGER.info('processing: %r -> %r', input_file, output_file)
+    root = parse_xml(input_file)
+    fix_jats_xml_node(root)
+    Path(output_file).parent.mkdir(parents=True, exist_ok=True)
+    Path(output_file).write_bytes(etree.tostring(root))
+
+
+def add_args(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        '--input', type=str, required=True,
+        help='path to input xml file'
+    )
+    parser.add_argument(
+        '--output', type=str, required=True,
+        help='path to output xml file'
+    )
+
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser()
+    add_args(parser)
+    add_debug_argument(parser)
+
+    parsed_args = parser.parse_args(argv)
+    LOGGER.info('parsed_args: %s', parsed_args)
+    return parsed_args
+
+
+def run(args: argparse.Namespace):
+    fix_jats_xml_file(
+        args.input,
+        args.output
+    )
+
+
+def main(argv=None):
+    args = parse_args(argv)
+    process_debug_argument(args)
+    run(args)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level='INFO')
+
+    main()
--- tests/fix_jats_xml_test.py
+++ tests/fix_jats_xml_test.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import List
+
 from lxml import etree
 from lxml.builder import E
 
@@ -6,8 +9,13 @@ from sciencebeam_utils.utils.xml import (
     get_text_content_list
 )
 
+from sciencebeam_trainer_grobid_tools.utils.xml import (
+    parse_xml
+)
+
 from sciencebeam_trainer_grobid_tools.fix_jats_xml import (
-    fix_doi
+    fix_doi,
+    main
 )
 
 
@@ -28,6 +36,12 @@ def get_jats_doi(doi: str) -> etree.Element:
 
 def clone_node(node: etree.Element) -> etree.Element:
     return etree.fromstring(etree.tostring(node))
+
+
+def get_jats(references: List[etree.Element]) -> etree.Element:
+    return E.article(E.back(
+        E('ref-list', *references)
+    ))
 
 
 class TestFixDoi:
@@ -100,3 +114,22 @@ class TestFixDoi:
         fixed_doi = '|'.join(get_text_content_list(fixed_ref.xpath(DOI_XPATH)))
         assert fixed_doi == DOI_1
         assert get_text_content(fixed_ref) == original_ref_text
+
+
+class TestMain:
+    def test_should_fix_jats_xml(self, temp_dir: Path):
+        original_ref = get_jats_mixed_ref('doi: ', get_jats_doi(HTTPS_DOI_URL_PREFIX + DOI_1))
+        input_file = temp_dir / 'input' / 'file1.xml'
+        input_file.parent.mkdir()
+        input_file.write_bytes(etree.tostring(get_jats(references=[
+            original_ref
+        ])))
+        output_file = temp_dir / 'output' / 'file1.xml'
+        main([
+            '--input=%s' % input_file,
+            '--output=%s' % output_file
+        ])
+        assert output_file.exists()
+        fixed_root = parse_xml(str(output_file))
+        fixed_doi = '|'.join(get_text_content_list(fixed_root.xpath(DOI_XPATH)))
+        assert fixed_doi == DOI_1
