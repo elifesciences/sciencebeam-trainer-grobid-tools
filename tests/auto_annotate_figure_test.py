e708062a240a8dff140ef6a861600741bf946f7c
Daniel Ecer <de-code@users.noreply.github.com>
2020-11-27 14:36:10 +0000

    added test for figure segmentation

--- tests/auto_annotate_figure_test.py
+++ tests/auto_annotate_figure_test.py
@@ -7,6 +7,8 @@ import pytest
 from lxml import etree
 from lxml.builder import E
 
+from sciencebeam_utils.utils.xml import get_text_content_list
+
 from sciencebeam_trainer_grobid_tools.utils.tei_xml import (
     get_tei_xpath_matches,
     get_tei_xpath_text_list,
@@ -136,4 +138,87 @@ class TestEndToEnd(object):
         ]
         assert get_tei_xpath_text_list(tei_auto_root, './/figDesc') == [
             TEXT_1, TEXT_2
+        ]
+
+    def test_should_not_segment_figures_if_disabled(
+            self, test_helper: SingleFileAutoAnnotateEndToEndTestHelper):
+        target_figure_content_nodes_1 = [
+            E.label(LABEL_1),
+            ' ',
+            E.caption(E.p(TEXT_1))
+        ]
+        target_figure_content_nodes_2 = [
+            E.label(LABEL_2),
+            ' ',
+            E.caption(E.p(TEXT_2))
+        ]
+        target_jats_xml = etree.tostring(
+            get_target_xml_node(body_nodes=[
+                E.fig(*target_figure_content_nodes_1),
+                E.fig(*target_figure_content_nodes_2)
+            ])
+        )
+        test_helper.tei_raw_file_path.write_bytes(etree.tostring(
+            get_training_tei_node([
+                E.figure(get_nodes_text(
+                    target_figure_content_nodes_1
+                    + [' ']
+                    + target_figure_content_nodes_2
+                ))
+            ])
+        ))
+        LOGGER.debug('target_jats_xml: %s', target_jats_xml)
+        test_helper.xml_file_path.write_bytes(target_jats_xml)
+        main(dict_to_args({
+            **test_helper.main_args_dict,
+            'matcher': 'simple',
+            'fields': 'figure'
+        }), save_main_session=False)
+
+        tei_auto_root = test_helper.get_tei_auto_root()
+        assert get_text_content_list(get_all_figures(tei_auto_root)) == [
+            get_nodes_text(target_figure_content_nodes_1)
+            + ' ' + get_nodes_text(target_figure_content_nodes_2)
+        ]
+
+    def test_should_segment_figures_if_enabled(
+            self, test_helper: SingleFileAutoAnnotateEndToEndTestHelper):
+        target_figure_content_nodes_1 = [
+            E.label(LABEL_1),
+            ' ',
+            E.caption(E.p(TEXT_1))
+        ]
+        target_figure_content_nodes_2 = [
+            E.label(LABEL_2),
+            ' ',
+            E.caption(E.p(TEXT_2))
+        ]
+        target_jats_xml = etree.tostring(
+            get_target_xml_node(body_nodes=[
+                E.fig(*target_figure_content_nodes_1),
+                E.fig(*target_figure_content_nodes_2)
+            ])
+        )
+        test_helper.tei_raw_file_path.write_bytes(etree.tostring(
+            get_training_tei_node([
+                E.figure(get_nodes_text(
+                    target_figure_content_nodes_1
+                    + [' ']
+                    + target_figure_content_nodes_2
+                ))
+            ])
+        ))
+        LOGGER.debug('target_jats_xml: %s', target_jats_xml)
+        test_helper.xml_file_path.write_bytes(target_jats_xml)
+        main(dict_to_args({
+            **test_helper.main_args_dict,
+            'matcher': 'simple',
+            'segment-figures': True,
+            'fields': 'figure'
+        }), save_main_session=False)
+
+        tei_auto_root = test_helper.get_tei_auto_root()
+        assert get_text_content_list(get_all_figures(tei_auto_root)) == [
+            get_nodes_text(target_figure_content_nodes_1),
+            get_nodes_text(target_figure_content_nodes_2)
         ]
