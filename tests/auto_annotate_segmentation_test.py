6089f1d1a6642ee6be172312e746e93007d7118c
Daniel Ecer <de-code@users.noreply.github.com>
2021-01-21 21:48:09 +0000

    added tests for top level paragraphs, list items and acknowledgements

--- tests/auto_annotate_segmentation_test.py
+++ tests/auto_annotate_segmentation_test.py
@@ -141,11 +141,10 @@ class TestEndToEnd(object):
                 E.p(TEXT_2)
             )
         ]
-        tei_text = get_nodes_text(
-            target_body_content_nodes
-            + ['\n']
-            + target_back_content_nodes
-        )
+        tei_text = '\n'.join([
+            get_nodes_text(target_body_content_nodes),
+            get_nodes_text(target_back_content_nodes)
+        ])
         LOGGER.debug('tei_text: %s', tei_text)
         test_helper.tei_raw_file_path.write_bytes(etree.tostring(
             get_training_tei_node(get_tei_nodes_for_text(tei_text))
@@ -173,6 +172,101 @@ class TestEndToEnd(object):
                 TEXT_1
             ])
         ]
+        assert get_xpath_text_list(tei_auto_root, '//back') == [
+            '\n'.join([
+                SECTION_TITLE_2,
+                TEXT_2
+            ])
+        ]
+
+    def test_should_auto_annotate_body_and_back_top_level_section_paragraphs(
+            self, test_helper: SingleFileAutoAnnotateEndToEndTestHelper):
+        target_body_content_nodes = [E.p(TEXT_1)]
+        target_back_content_nodes = [E.p(TEXT_2)]
+        tei_text = '\n'.join([
+            get_nodes_text(target_body_content_nodes),
+            get_nodes_text(target_back_content_nodes)
+        ])
+        LOGGER.debug('tei_text: %s', tei_text)
+        test_helper.tei_raw_file_path.write_bytes(etree.tostring(
+            get_training_tei_node(get_tei_nodes_for_text(tei_text))
+        ))
+        test_helper.xml_file_path.write_bytes(etree.tostring(
+            get_target_xml_node(
+                body_nodes=target_body_content_nodes,
+                back_nodes=target_back_content_nodes
+            )
+        ))
+        main(dict_to_args({
+            **test_helper.main_args_dict,
+            'fields': ','.join([
+                'body_section_paragraph',
+                'back_section_paragraph'
+            ])
+        }), save_main_session=False)
+
+        tei_auto_root = test_helper.get_tei_auto_root()
+        assert get_xpath_text_list(tei_auto_root, '//body') == [TEXT_1]
+        assert get_xpath_text_list(tei_auto_root, '//back') == [TEXT_2]
+
+    def test_should_auto_annotate_body_and_back_list_item_section_paragraphs(
+            self, test_helper: SingleFileAutoAnnotateEndToEndTestHelper):
+        target_body_content_nodes = [E.list(E('list-item', E.p(TEXT_1)))]
+        target_back_content_nodes = [E.list(E('list-item', E.p(TEXT_2)))]
+        tei_text = '\n'.join([
+            get_nodes_text(target_body_content_nodes),
+            get_nodes_text(target_back_content_nodes)
+        ])
+        LOGGER.debug('tei_text: %s', tei_text)
+        test_helper.tei_raw_file_path.write_bytes(etree.tostring(
+            get_training_tei_node(get_tei_nodes_for_text(tei_text))
+        ))
+        test_helper.xml_file_path.write_bytes(etree.tostring(
+            get_target_xml_node(
+                body_nodes=target_body_content_nodes,
+                back_nodes=target_back_content_nodes
+            )
+        ))
+        main(dict_to_args({
+            **test_helper.main_args_dict,
+            'fields': ','.join([
+                'body_section_paragraph',
+                'back_section_paragraph'
+            ])
+        }), save_main_session=False)
+
+        tei_auto_root = test_helper.get_tei_auto_root()
+        assert get_xpath_text_list(tei_auto_root, '//body') == [TEXT_1]
+        assert get_xpath_text_list(tei_auto_root, '//back') == [TEXT_2]
+
+    def test_should_auto_annotate_acknowledgment_section_as_back(
+            self, test_helper: SingleFileAutoAnnotateEndToEndTestHelper):
+        target_back_content_nodes = [
+            E.ack(
+                E.title(SECTION_TITLE_2),
+                E.lb(),
+                '\n',
+                E.p(TEXT_2)
+            )
+        ]
+        tei_text = get_nodes_text(target_back_content_nodes)
+        test_helper.tei_raw_file_path.write_bytes(etree.tostring(
+            get_training_tei_node(get_tei_nodes_for_text(tei_text))
+        ))
+        test_helper.xml_file_path.write_bytes(etree.tostring(
+            get_target_xml_node(back_nodes=target_back_content_nodes)
+        ))
+        main(dict_to_args({
+            **test_helper.main_args_dict,
+            'fields': ','.join([
+                'body_section_title',
+                'body_section_paragraph',
+                'back_section_title',
+                'back_section_paragraph'
+            ])
+        }), save_main_session=False)
+
+        tei_auto_root = test_helper.get_tei_auto_root()
         assert get_xpath_text_list(tei_auto_root, '//back') == [
             '\n'.join([
                 SECTION_TITLE_2,
