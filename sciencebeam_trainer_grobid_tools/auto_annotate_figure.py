19bec3172f752c15edddc0ae85a00d8c37529826
Daniel Ecer <de-code@users.noreply.github.com>
2021-02-16 18:44:04 +0000

    linting

--- sciencebeam_trainer_grobid_tools/auto_annotate_figure.py
+++ sciencebeam_trainer_grobid_tools/auto_annotate_figure.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import argparse
 import logging
 import os
-from contextlib import contextmanager, ExitStack
+from contextlib import contextmanager
 from tempfile import TemporaryDirectory
 from typing import ContextManager
 
