744ef27bb45085caea9895ea348a2816c50d8888
Daniel Ecer <de-code@users.noreply.github.com>
2021-05-27 18:52:09 +0100

    assert GrobidTrainingTeiStructuredDocument

--- sciencebeam_trainer_grobid_tools/annotation/remove_untagged_annotator.py
+++ sciencebeam_trainer_grobid_tools/annotation/remove_untagged_annotator.py
@@ -4,9 +4,13 @@ from sciencebeam_trainer_grobid_tools.core.structured_document import (
 from sciencebeam_trainer_grobid_tools.core.annotation.annotator import (
     AbstractAnnotator
 )
+from sciencebeam_trainer_grobid_tools.structured_document.grobid_training_tei import (
+    GrobidTrainingTeiStructuredDocument
+)
 
 
 class RemoveUntaggedPostProcessingAnnotator(AbstractAnnotator):
     def annotate(self, structured_document: AbstractStructuredDocument):
+        assert isinstance(structured_document, GrobidTrainingTeiStructuredDocument)
         structured_document.remove_all_untagged()
         return structured_document
